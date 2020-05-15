ab52bcbc6dd2fa90bec5089f9f1997b551b312ce
gux <gux.vega@gmail.com>
2020-05-15 12:58:17 -0600

    Updates

---
+++ precache-manifest.65cc172708c0592b012291f83ed5bfe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd191da15291dadb2a3a832a14a0b69",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "9774031ec8f09daeca6d",
+    "url": "/monsters/static/css/main.acd60de4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9774031ec8f09daeca6d",
+    "url": "/monsters/static/js/main.1c8395c3.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
